c049b48456474eb0031f825ba6970e69ec1a3ec5
Joaquín Gómez <142223436+Joagz@users.noreply.github.com>
2025-05-10 20:06:12 -0300

    Initialization Essentials For Ubuntu 22.04 WITH ROS2

---
+++ LinuxInstallationEssentials.sh
@@ -0,0 +1,79 @@
+# Install VsCode
+
+echo "Downloading Visual Studio Code .deb package from https://go.microsoft.com/fwlink/?LinkID=760868"
+echo "..."
+
+file_name=$(wget -nv -t 20 --content-disposition "https://go.microsoft.com/fwlink/?LinkID=760868"  2>&1 | cut -d\" -f2)
+
+echo "Successfully downloded $file_name"
+
+sudo apt install ./$file_name
+
+echo "code code/add-microsoft-repo boolean true" | sudo debconf-set-selections
+
+sudo apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+rm -f packages.microsoft.gpg
+
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code # or code-insiders
+
+echo "\n\n Installing ROS2 Humble Hawksbill, Texlive, Python3, CMake and Git/GitCli\n\n"
+
+locale  # check for UTF-8
+
+sudo apt update && sudo apt install locales
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+locale  # verify settings
+
+sudo apt install software-properties-common
+sudo add-apt-repository universe
+
+sudo apt update && sudo apt install curl -y
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+# Install Ros and other packages like TexLive
+sudo apt update && sudo apt install -y \
+  python3-flake8-docstrings \
+  python3-pip \
+  python3-pytest-cov \
+  ros-dev-tools \ 
+  octave \
+  python3 \
+  texlive-full \
+  build-essentials \
+  cmake \
+  git \
+  gh \
+  python3-flake8-blind-except \
+  python3-flake8-builtins \
+  python3-flake8-class-newline \
+  python3-flake8-comprehensions \
+  python3-flake8-deprecated \
+  python3-flake8-import-order \
+  python3-flake8-quotes \
+  python3-pytest-repeat \
+  python3-pytest-rerunfailures
+
+mkdir -p ~/ros2_humble/src
+cd ~/ros2_humble
+vcs import --input https://raw.githubusercontent.com/ros2/ros2/humble/ros2.repos src
+
+sudo apt upgrade
+
+sudo rosdep init
+rosdep update
+rosdep install --from-paths src --ignore-src -y --skip-keys "fastcdr rti-connext-dds-6.0.1 urdfdom_headers"
+
+echo "source . ~/ros2_humble/install/local_setup.bash" >> ~/.bashrc
+
+echo "Installations completed successfully!! -- Joaquín"
+
