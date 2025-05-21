d42abe888ebb3d03d97ea3ceb4e22969e61c322a
Joaquín Gómez <142223436+Joagz@users.noreply.github.com>
2025-05-21 16:05:13 -0300

    Update LinuxInstallationEssentials.sh

--- LinuxInstallationEssentials.sh
+++ LinuxInstallationEssentials.sh
@@ -84,6 +84,9 @@ sudo curl https://packages.osrfoundation.org/gazebo.gpg --output /usr/share/keyr
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/pkgs-osrf-archive-keyring.gpg] http://packages.osrfoundation.org/gazebo/ubuntu-stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/gazebo-stable.list > /dev/null
 sudo apt-get update
 sudo apt-get install ignition-fortress
+
+sudo apt-get install ros-humble-ros-ign-bridge
+echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
 fi
 
 # ==============================
